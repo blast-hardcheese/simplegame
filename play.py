5aaf8d64e6b82a2b08b5a3ee50a04f0616e3a587
Devon Stewart <blast@hardchee.se>
2013-06-25 21:02:12 +0900

    Adding some notes

--- play.py
+++ play.py
@@ -3,6 +3,8 @@
 from core.main import Game
 from core.levels.level001 import myMap
 
+# Currently you can return one of the four movement directions, L, R, U, D
+# Returning anything else skips one move
 def level001(state):
     print state
     return ''
