c9f1aab9b8e04adb895849e7ca443779951c894b
Devon Stewart <blast@hardchee.se>
2013-06-25 22:43:05 +0900

    Adding an example for using state['look']

--- play.py
+++ play.py
@@ -2,11 +2,13 @@
 
 from core.main import Game
 from core.levels.level001 import myMap
+from core.types import Space,Wall,Start,End
 
 # Currently you can return one of the four movement directions, L, R, U, D
 # Returning anything else skips one move
 def level001(state):
     print state
+    print "Is there a space above me?", state['look']('U') == Space
     return ''
 
 game = Game()
